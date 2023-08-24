3e0258152d099cca9269a21f77a2cf3625045778
laurenzooz <lauri.pahkala@hotmail.com>
2023-08-24 23:10:09 +0300

    syntax fix

--- flyway/sql/V1___initial_schema.sql
+++ flyway/sql/V1___initial_schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   email VARCHAR(255) UNIQUE,
-  password CHAR(60),
+  password CHAR(60)
 );
 
 CREATE TABLE shopping_lists (
--- shopping-list/services/listService.js
+++ shopping-list/services/listService.js
@@ -2,7 +2,7 @@ import {sql} from "../database/database.js";
 
 
 //const addList = async(user_id, name) => {
-const addList = async(user_id, name) => { // user id temporarily disabled
+const addList = async(name) => { // user id temporarily disabled
 	//await sql `INSERT INTO shopping_lists (user_id, name) VALUES (0, ${name})`;
 	await sql `INSERT INTO shopping_lists (name) VALUES (${name})`;
 }
