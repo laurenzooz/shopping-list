c9c4f83929cc81e0e55df2be38fb3c3281b0d3cd
laurenzooz <lauri.pahkala@hotmail.com>
2023-08-28 11:19:48 +0300

    user-specific lists,  can only delete own list

--- shopping-list/routes/controllers/listController.js
+++ shopping-list/routes/controllers/listController.js
@@ -1,24 +1,25 @@
 import * as listService from  "../../services/listService.js";
 
-const showLists = async ({ render }) => {
-	render("lists.eta", {lists: await listService.listLists()});
+const showLists = async ({ render, user }) => {
+	render("lists.eta", {lists: await listService.listLists(user.id)});
 }
 
-const addList = async ({ request, response, params, render }) => {
+const addList = async ({ request, response, user, params, render }) => {
 
 	// name of the new shopping list
 	const body = request.body({ type: "form" });
 	const formData = await body.value;
 	const listName = formData.get("list_name");
 	
-	await listService.addList(listName);
+	await listService.addList(user.id, listName);
 
 	response.redirect(`/lists`);
 }
 	
-const deleteList = async ({ request, response, params, render }) => {
+const deleteList = async ({ request, response, params, render, user }) => {
 	
-	await listService.deleteList(params.id);
+
+	await listService.deleteList(params.id, user.id);
  
 	response.redirect(`/lists`);
 }
--- shopping-list/services/listService.js
+++ shopping-list/services/listService.js
@@ -1,20 +1,25 @@
 import {sql} from "../database/database.js";
 
-//const addList = async(user_id, name) => {
-const addList = async(name) => { // user id temporarily disabled
-	//await sql `INSERT INTO shopping_lists (user_id, name) VALUES (0, ${name})`;
-	await sql `INSERT INTO shopping_lists (name) VALUES (${name})`;
+const addList = async(user_id, name) => {
+	await sql `INSERT INTO shopping_lists (user_id, name) VALUES (${user_id}, ${name})`;
 }
 
-const listLists = async(name) => { // returns all of the shopping lists of the user
-	return await sql `SELECT * FROM shopping_lists`;
+const listLists = async(user_id) => { // returns all of the shopping lists of the user
+	return await sql `SELECT * FROM shopping_lists WHERE user_id = ${user_id}`;
 }
 
 
-const deleteList = async(id) => { // deletes items in the list first, then the list
+const deleteList = async(list_id, user_id) => { 
+	// deletes items in the list first, then the list
 
-	await sql `DELETE FROM shopping_list_items WHERE shopping_list_id = ${id}`;
-	await sql `DELETE FROM shopping_lists WHERE id = ${id}`;
+	// check that the user id matches
+	const list = await sql `SELECT * FROM shopping_lists WHERE id = ${list_id}`;
+	
+	if (list[0].user_id === user_id) {
+		await sql `DELETE FROM shopping_list_items WHERE shopping_list_id = ${list_id}`;
+		await sql `DELETE FROM shopping_lists WHERE id = ${list_id}`;
+	}
+	
 }
 
 export { addList, listLists, deleteList }
