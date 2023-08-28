fc2782cebd2e5564242c6937a071cb9804220e45
laurenzooz <lauri.pahkala@hotmail.com>
2023-08-28 10:57:56 +0300

    added user id columns to shopping_lists and items

--- flyway/sql/V1___initial_schema.sql
+++ flyway/sql/V1___initial_schema.sql
@@ -1,19 +1,21 @@
 CREATE TABLE users (
-  id SERIAL PRIMARY KEY,
-  email VARCHAR(255) UNIQUE,
-  password CHAR(60)
+	id SERIAL PRIMARY KEY,
+	email VARCHAR(255) UNIQUE,
+	password CHAR(60)
 );
 
 CREATE TABLE shopping_lists (
-  id SERIAL PRIMARY KEY,
-  name TEXT NOT NULL
+	id SERIAL PRIMARY KEY,
+	user_id INTEGER REFERENCES users(id),
+	name TEXT NOT NULL
 );
 
 CREATE TABLE shopping_list_items (
-  id SERIAL PRIMARY KEY,
-  shopping_list_id INTEGER REFERENCES shopping_lists(id),
-  name TEXT NOT NULL,
-  collected BOOLEAN DEFAULT FALSE
+	id SERIAL PRIMARY KEY,
+	shopping_list_id INTEGER REFERENCES shopping_lists(id),
+	user_id INTEGER REFERENCES users(id),
+	name TEXT NOT NULL,
+	collected BOOLEAN DEFAULT FALSE
 );
 
 CREATE UNIQUE INDEX ON users((lower(email)));
