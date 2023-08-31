6a0d025d9ccc96472d6d709dca48045cf8a71501
laurenzooz <lauri.pahkala@hotmail.com>
2023-08-31 22:47:35 +0300

    servestatic middleware added

--- shopping-list/app.js
+++ shopping-list/app.js
@@ -2,6 +2,7 @@ import { Application, Session } from "./deps.js";
 import { errorMiddleware } from "./middlewares/errorMiddleware.js";
 import { renderMiddleware } from "./middlewares/renderMiddleware.js";
 import { authMiddleware } from "./middlewares/authMiddleware.js";
+import { serveStaticFiles } from "./middlewares/serveStaticMiddleware.js";
 import { router } from "./routes/routes.js";
 
 
@@ -11,6 +12,7 @@ app.use(Session.initMiddleware());
 app.use(errorMiddleware);
 app.use(renderMiddleware);
 app.use(authMiddleware);
+app.use(serveStaticFiles);
 app.use(router.routes());
 
 app.listen({ port: 7777 });
