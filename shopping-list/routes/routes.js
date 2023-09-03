3e8d1b3adb1ed1d073ebc99872e5e6db47eb1895
laurenzooz <lauri.pahkala@hotmail.com>
2023-09-03 11:53:07 +0300

    incourrect path fixed

--- shopping-list/routes/routes.js
+++ shopping-list/routes/routes.js
@@ -31,6 +31,6 @@ router.post("/lists/:list_id/:item_id/collect", itemController.collectItem);
 router.get("/lists/:list_id/:item_id/uncollect", itemController.uncollectItem); 
 router.post("/lists/:list_id/:item_id/delete", itemController.deleteItem); 
 
-router.post("/lists/:list_id/:item_id/order", itemController.orderItems);
+router.post("/lists/:list_id/order", itemController.orderItems);
  
 export { router };
