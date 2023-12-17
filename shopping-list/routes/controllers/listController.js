7b6195a531db9b670c97a96a80462b5807858fe0
laurenzooz <lauri.pahkala@hotmail.com>
2023-12-17 14:33:28 +0200

    Added validation to list name (can't be empty)

--- shopping-list/routes/controllers/listController.js
+++ shopping-list/routes/controllers/listController.js
@@ -1,19 +1,35 @@
 import * as listService from  "../../services/listService.js";
+import { minLength, required, validate, invalid } from "https://deno.land/x/validasaur@v0.15.0/mod.ts";
+
 
 const showLists = async ({ render, user }) => {
-	render("lists.eta", {lists: await listService.listLists(user.id), is_logged: (user)});
+	let data = { name: "", errors: null, lists: await listService.listLists(user.id), is_logged: (user)};
+	render("lists.eta", data);
 }
+
+const validationRules = { name: [required, minLength(1)] };
 
 const addList = async ({ request, response, user, params, render }) => {
 
 	// name of the new shopping list
 	const body = request.body({ type: "form" });
 	const formData = await body.value;
+	
 	const listName = formData.get("list_name");
 
-	await listService.addList(user.id, listName);
+	let data = { name: listName, errors: null, lists: await listService.listLists(user.id), is_logged: (user)};
+	const [passes, errors] = await validate(data, validationRules);
 
-	response.redirect(`/lists`);
+	if (!passes) { 
+		data.errors = errors;
+		render("lists.eta", data);
+	}
+	else {
+		await listService.addList(user.id, listName);
+		response.redirect(`/lists`);
+	}
+
+	
 }
 	
 const deleteList = async ({ request, response, params, render, user }) => {
