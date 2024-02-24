55e1f6be564ed913adaa46f4fc5e9090cadeeb41
laurenzooz <lauri.pahkala@hotmail.com>
2024-02-24 15:15:19 +0200

    Update test

--- e2e-playwright/tests/shopping-list.spec.js
+++ e2e-playwright/tests/shopping-list.spec.js
@@ -1,7 +1,7 @@
 const { test, expect } = require("@playwright/test");
 
 test("Has expected title", async ({ page }) => {
-    await page.goto("/");
+    await page.goto("http://localhost:7777/");
     await expect(page).toHaveTitle("Shopping list");
 });
 
